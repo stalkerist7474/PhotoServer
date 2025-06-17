aef1cb69d63ded495021139a3c1886b16e748700
stalkerist7474 <stalkerist7474@gmail.com>
2025-06-17 17:46:48 +0300

    base

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Hello"
+    return render_template("index.html")
